d78b86d7d14cf874b743b702692332636847021d
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-08-23 16:49:26 -0300

    feat: configure personal release torrent skip (#383)

--- src/configvalidator.py
+++ src/configvalidator.py
@@ -87,6 +87,7 @@ DEFAULT_KEY_TYPES: dict[str, tuple[type, ...]] = {
     "queue_prepare_concurrency": (str, int),
     "unit3d_dupe_max_pages": (str, int),
     "skip_auto_torrent": (bool,),
+    "skip_auto_torrent_personalrelease": (bool,),
     "sfx_on_prompt": (bool,),
     "console_show_time": (bool,),
     "console_show_level": (bool,),
