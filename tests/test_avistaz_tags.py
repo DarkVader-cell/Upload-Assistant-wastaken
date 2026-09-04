910b0251c219452c73202da6ca737d444a8835ad
FortKnox1337 <34667229+FortKnox1337@users.noreply.github.com>
2026-09-04 13:57:05 +0100

    fix(avistaz): add release tags when keywords are empty (#451)

--- tests/test_avistaz_tags.py
+++ tests/test_avistaz_tags.py
@@ -7,7 +7,11 @@ from src.trackers.AVISTAZ.avistaz import AvistaZ
 from src.trackers.AVISTAZ.cinemaz import CinemaZ
 from src.trackers.AVISTAZ.privatehd import PrivateHD
 
-TRACKERS = ((AvistaZ, "AVISTAZ", "3773", "943"), (CinemaZ, "CINEMAZ", "1594", "938"), (PrivateHD, "PRIVATEHD", "1448", "415"))
+TRACKERS = (
+    (AvistaZ, "AVISTAZ", "3773", "943"),
+    (CinemaZ, "CINEMAZ", "1594", "938"),
+    (PrivateHD, "PRIVATEHD", "1448", "415"),
+)
 
 
 async def get_tags(tracker, meta):
@@ -19,11 +23,17 @@ async def get_tags(tracker, meta):
 
 @pytest.mark.parametrize(("tracker_class", "tracker_name", "personal_tag", "_internal_tag"), TRACKERS)
 def test_personal_release_tag_is_added_without_keywords(tracker_class, tracker_name, personal_tag, _internal_tag):
-    tags = asyncio.run(get_tags(tracker_class({"TRACKERS": {tracker_name: {}}}), SimpleNamespace(keywords=[], personalrelease=True)))
+    tracker = tracker_class({"TRACKERS": {tracker_name: {}}})
+    meta = SimpleNamespace(keywords=[], personalrelease=True)
+
+    tags = asyncio.run(get_tags(tracker, meta))
     assert tags == [personal_tag]  # noqa: S101
 
 
 @pytest.mark.parametrize(("tracker_class", "tracker_name", "_personal_tag", "internal_tag"), TRACKERS)
 def test_internal_release_tag_is_added_without_keywords(tracker_class, tracker_name, _personal_tag, internal_tag):
-    tags = asyncio.run(get_tags(tracker_class({"TRACKERS": {tracker_name: {"internal": True}}}), SimpleNamespace(keywords=[], personalrelease=False)))
+    tracker = tracker_class({"TRACKERS": {tracker_name: {"internal": True}}})
+    meta = SimpleNamespace(keywords=[], personalrelease=False)
+
+    tags = asyncio.run(get_tags(tracker, meta))
     assert tags == [internal_tag]  # noqa: S101
